3e1b427b5afb40a94bd7a1707ec3ead6be46b1dd
dongxiangxiang <dongxiangxiang@cityhouse.cn>
2018-10-11 14:41:45 +0800

    doze
    电量白名单检测

--- AndroidHighPerformance/app/src/main/java/com/vincent/high/performance/activity/MainActivity.java
+++ AndroidHighPerformance/app/src/main/java/com/vincent/high/performance/activity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.vincent.high.performance.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.vincent.high.performance.R;
+import com.vincent.high.performance.battery.BatterySkills;
 import com.vincent.high.performance.entity.News;
 import com.vincent.high.performance.other.OnNewsSelected;
 
-public class MainActivity extends BaseActivity implements OnNewsSelected {
+public class MainActivity extends BaseActivity implements OnNewsSelected, View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,51 @@ public class MainActivity extends BaseActivity implements OnNewsSelected {
             public void run() {
                 setTitle("aaaaaaa");
             }
-        },100000);
+        }, 100000);
+
+        findViewById(R.id.btn_battery).setOnClickListener(this);
     }
 
     @Override
     public void onNewsClick(News news) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            /**
+             * doze模式下 设置电量优化白名单
+             */
+            case R.id.btn_battery:
+                boolean isInWhiteList = BatterySkills.isIgnoringBatteryOptimizations(this);
+                if (!isInWhiteList) {
+                    BatterySkills.requestIgnoreBatteryOptimizations(this);
+                } else {
+                    toast("已在电量优化白名单中");
+                }
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == BatterySkills.REQUEST_IGNORE_BATTERY_CODE) {
+            if (requestCode == RESULT_OK) {
+                toast("设置电量优化白名单成功");
+            } else {
+                toast("设置电量优化白名单失败");
+            }
+        }
+    }
+
+
+    private void toast(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 }
--- AndroidHighPerformance/app/src/main/java/com/vincent/high/performance/battery/BatterySkills.java
+++ AndroidHighPerformance/app/src/main/java/com/vincent/high/performance/battery/BatterySkills.java
@@ -1,10 +1,19 @@
 package com.vincent.high.performance.battery;
 
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.BatteryManager;
+import android.os.Build;
+import android.os.PowerManager;
+import android.provider.Settings;
 
 public class BatterySkills {
+
+    public static final int REQUEST_IGNORE_BATTERY_CODE = 10002;
 
     /**
      * 获取手机电量
@@ -17,5 +26,53 @@ public class BatterySkills {
         int scale = intentBatteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         float batteryPercentage = level / (float) scale;
         return batteryPercentage;
+    }
+
+
+    /**
+     * 判断是否是在电量优化白名单上
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isIgnoringBatteryOptimizations(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String packageName = context.getPackageName();
+            PowerManager pm = (PowerManager) context
+                    .getSystemService(Context.POWER_SERVICE);
+            if (pm.isIgnoringBatteryOptimizations(packageName)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 请求加入白名单
+     * 需要权限
+     *
+     * <uses-permission android:name="android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS"/>
+     *
+     * @param activity
+     */
+    public static void requestIgnoreBatteryOptimizations(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                Intent intent = new Intent();
+                String packageName = activity.getPackageName();
+                PowerManager pm = (PowerManager) activity.getSystemService(Context.POWER_SERVICE);
+                if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+//               intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
+                    intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                    intent.setData(Uri.parse("package:" + packageName));
+                    activity.startActivityForResult(intent, REQUEST_IGNORE_BATTERY_CODE);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
